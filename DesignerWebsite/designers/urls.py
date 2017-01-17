9a88343ef6d866ea79ed323c5b89ad66a87f80d9
tonmoysagar <tonmoysagar@gmail.com>
2017-01-17 17:35:51 +0530

    login corrected

--- DesignerWebsite/designers/urls.py
+++ DesignerWebsite/designers/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from django.views.generic import TemplateView
 from . import views
 
+app_name='designers'
+
 urlpatterns = [
     url(r'^$', views.index,name='index'),
     url(r'register/$',views.DesignerCreate.as_view(),name='register'),
