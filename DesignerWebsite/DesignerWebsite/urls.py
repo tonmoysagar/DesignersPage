79f254bcb90fc53f3a4e72490f348e3f6bc55c64
tonmoysagar <tonmoysagar@gmail.com>
2017-01-17 18:07:18 +0530

    Login page correction 2

--- DesignerWebsite/DesignerWebsite/urls.py
+++ DesignerWebsite/DesignerWebsite/urls.py
@@ -1,7 +1,23 @@
-from django.conf.urls import url,include
+from django.conf.urls import url,include,patterns
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+
+def my_view(request):
+    return HttpResponse("Hello!")
+
+def get_admin_urls(urls):
+    def get_urls():
+        my_urls = patterns('',
+            (r'^my_view/$', admin.site.admin_view(my_view))
+        )
+        return my_urls + urls
+    return get_urls
+
+admin_urls = get_admin_urls(admin.site.get_urls())
+admin.site.get_urls = admin_urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
